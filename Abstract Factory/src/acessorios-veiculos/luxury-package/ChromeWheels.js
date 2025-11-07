01e91e3aa12dcaee700f896fa1d96e65464cdc12
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2025-11-07 07:08:52 -0300

    .

---
+++ Abstract Factory/src/acessorios-veiculos/luxury-package/ChromeWheels.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ChromeWheels = void 0;
+// concrete class
+var ChromeWheels = /** @class */ (function () {
+    function ChromeWheels() {
+    }
+    ChromeWheels.prototype.type = function () {
+        return 'Chrome wheels type.';
+    };
+    return ChromeWheels;
+}());
+exports.ChromeWheels = ChromeWheels;
