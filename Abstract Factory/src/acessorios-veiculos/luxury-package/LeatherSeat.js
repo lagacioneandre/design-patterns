94657931920bd80d1cb55fb9fa602c4377c44ec9
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2025-11-09 10:55:13 -0300

    .

---
+++ Abstract Factory/src/acessorios-veiculos/luxury-package/LeatherSeat.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LeatherSeat = void 0;
+// concrete class
+var LeatherSeat = /** @class */ (function () {
+    function LeatherSeat() {
+    }
+    LeatherSeat.prototype.description = function () {
+        return 'Leather seats.';
+    };
+    return LeatherSeat;
+}());
+exports.LeatherSeat = LeatherSeat;
