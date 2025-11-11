6d2e21ece551e2aa2993843e9c827676d7ba1d61
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2025-11-11 07:12:16 -0300

    .

---
+++ Abstract Factory/src/acessorios-veiculos/luxury-package/PanelTouch.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PanelTouch = void 0;
+// concrete class
+var PanelTouch = /** @class */ (function () {
+    function PanelTouch() {
+    }
+    PanelTouch.prototype.configuration = function () {
+        return 'Control panel touch screen.';
+    };
+    return PanelTouch;
+}());
+exports.PanelTouch = PanelTouch;
