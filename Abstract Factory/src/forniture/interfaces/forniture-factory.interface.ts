1fd6158a1bddcefd35ae90a5aee783efaa67eb4d
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2025-12-18 07:20:47 -0300

    .

--- Abstract Factory/src/forniture/interfaces/forniture-factory.interface.ts
+++
@@ -1,9 +0,0 @@
-import { IChair } from "../../chairs/interfaces/chair.interface";
-import { ISofa } from "../../sofa/interfaces/sofa.interface";
-import { ITable } from "../../table/interfaces/table.interface";
-
-export interface IFornitureFactory {
-    createChair(): IChair;
-    createSofa(): ISofa;
-    createTable(): ITable;
-}
