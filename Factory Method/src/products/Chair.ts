ecf0f91877b7e49035d5c234da0327bda789c32f
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2026-02-17 09:08:37 -0300

    .

--- Factory Method/src/products/Chair.ts
+++
@@ -1,7 +0,0 @@
-import { Product } from "../interfaces/product.interface";
-
-export class Chair implements Product {
-    public operation(): string {
-        return 'Result of Chair';
-    }
-}
--- Factory Method/src/products/Table.ts
+++
@@ -1,7 +0,0 @@
-import { Product } from "../interfaces/product.interface";
-
-export class Table implements Product {
-    public operation(): string {
-        return 'Rresult of Table';
-    }
-}
