b71d23c0fb158f71de0cd62d3360f63d5b21fa5b
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2026-02-25 09:55:22 -0300

    .

--- Prototype/src/ComponentWithBackReference.ts
+++
@@ -1,9 +0,0 @@
-import { Prototype } from "./Prototype";
-
-export class ComponentWithBackReference {
-    public prototype;
-
-    constructor(prototype: Prototype) {
-        this.prototype = prototype;
-    }
-}
