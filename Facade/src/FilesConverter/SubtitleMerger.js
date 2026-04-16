b219c303cd803f1db1c6bf214cf6e85403c9b0be
André Lagacione <151460799+lagacioneandre@users.noreply.github.com>
2026-04-16 07:42:20 -0300

    .

---
+++ Facade/src/FilesConverter/SubtitleMerger.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SubtitleMerger = void 0;
+var SubtitleMerger = /** @class */ (function () {
+    function SubtitleMerger() {
+    }
+    SubtitleMerger.prototype.merge = function (filePath) {
+        console.log("Adicionando legendas no arquivo ".concat(filePath));
+    };
+    return SubtitleMerger;
+}());
+exports.SubtitleMerger = SubtitleMerger;
